25a6e246b1cf38ce6376c3c33523201f15a17392
milinchuk <milinckukanastasia@gmail.com>
2015-03-22 21:33:27 +0200

    Simple Security

---
+++ src/main/java/com/travelit/secure/entity/enums/UserRoleEnum.java
@@ -0,0 +1,12 @@
+package com.travelit.secure.entity.enums;
+
+public enum UserRoleEnum {
+
+    ADMIN,
+    USER,
+    ANONYMOUS;
+
+    UserRoleEnum() {
+    }
+
+}
---
+++ src/main/java/com/travelit/secure/service/UserService.java
@@ -0,0 +1,9 @@
+package com.travelit.secure.service;
+
+import com.travelit.secure.entity.User;
+
+public interface UserService {
+
+    User getUser(String login);
+
+}
