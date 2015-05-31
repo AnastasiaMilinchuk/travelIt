3f6cfcafe8baf2428c1bfe8ca1d6a713eba07b0a
dzidzoiev <artur.dzidzoiev@gmail.com>
2015-05-31 14:19:30 +0300

    comparing users by prefs(to test)

--- src/main/java/com/travelit/secure/algorithms/similarity/faircompromise/FairCompromiseAlogo.java
+++ src/main/java/com/travelit/secure/algorithms/similarity/faircompromise/FairCompromiseAlogo.java
@@ -1,10 +1,9 @@
 package com.travelit.secure.algorithms.similarity.faircompromise;
 
 import com.travelit.secure.algorithms.similarity.PreferencesSimilarity;
-import com.travelit.secure.entity.Preference;
 import com.travelit.secure.entity.User;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by bubble on 31.05.15.
@@ -13,6 +12,30 @@ public class FairCompromiseAlogo implements PreferencesSimilarity {
 
     @Override
     public void sort(List<User> users, User userToCompare) {
-        List<Preference> preferences = userToCompare.getPreferences();
+        Map<User, Float> map = mapSimilarity(users, userToCompare);
+        Collections.sort(users, new UserComparator(map));
+    }
+
+    private Map<User, Float> mapSimilarity(List<User> users, User userToCompare) {
+        Map<User, Float> map = new HashMap<>(users.size(), 1);
+        JaccardIndexBasedSimilarity similarity = new JaccardIndexBasedSimilarity(userToCompare.getPreferences());
+        for (User user : users) {
+            map.put(user, similarity.getSimilarityRate(user.getPreferences()));
+        }
+        return map;
+    }
+
+    class UserComparator implements Comparator<User> {
+
+        private Map<User,Float> similarityMap;
+
+        public UserComparator(Map<User, Float> similarityMap) {
+            this.similarityMap = similarityMap;
+        }
+
+        @Override
+        public int compare(User o1, User o2) {
+            return similarityMap.get(o1).compareTo(similarityMap.get(o2));
+        }
     }
 }
--- src/main/java/com/travelit/secure/entity/User.java
+++ src/main/java/com/travelit/secure/entity/User.java
@@ -98,4 +98,20 @@ public class User  {
     public String toString(){
         return "Login: " + email + " Role: " + role;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return email.equals(user.email);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return email.hashCode();
+    }
 }
